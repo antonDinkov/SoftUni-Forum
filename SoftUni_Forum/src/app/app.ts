bf473fed920e86be598ed0ee76df92ddf16216fe
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-04 20:57:50 +0300

    basic templates ready

--- SoftUni_Forum/src/app/app.ts
+++ SoftUni_Forum/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
+import { ThemeBoard } from './features/themes';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, ThemeBoard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ SoftUni_Forum/src/app/features/themes/theme-board/theme-board.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ThemeItem } from '../theme-item/theme-item';
+
+@Component({
+  selector: 'app-theme-board',
+  imports: [ThemeItem],
+  templateUrl: './theme-board.html',
+  styleUrl: './theme-board.css'
+})
+export class ThemeBoard {
+
+}
