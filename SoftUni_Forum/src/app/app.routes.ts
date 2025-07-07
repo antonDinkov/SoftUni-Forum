99d7fe117701c58c7abb88f4676822c123bbab49
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-07 13:00:45 +0300

    login register & new templates ready

--- SoftUni_Forum/src/app/app.routes.ts
+++ SoftUni_Forum/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Catalog } from './features/catalog/catalog';
+import { NewTheme } from './features/new-theme/new-theme';
+import { Login } from './features/login/login';
+import { Register } from './features/register/register';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'catalog', component: Catalog }
+    { path: 'catalog', component: Catalog },
+    { path: 'new', component: NewTheme },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
 ];
---
+++ SoftUni_Forum/src/app/features/new-theme/new-theme.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-theme',
+  imports: [],
+  templateUrl: './new-theme.html',
+  styleUrl: './new-theme.css'
+})
+export class NewTheme {
+
+}
