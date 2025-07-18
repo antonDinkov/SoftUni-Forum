af6109b2ae8376fc8e193c899389d6f1f023f707
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-18 11:12:41 +0300

    404 & profile components + routing

--- SoftUni_Forum/src/app/app.routes.ts
+++ SoftUni_Forum/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './features/home/home';
-import { Catalog } from './features/catalog/catalog';
-import { NewTheme } from './features/new-theme/new-theme';
-import { Login } from './features/login/login';
-import { Register } from './features/register/register';
 
 export const routes: Routes = [
-    { path: '', component: Home },
-    { path: 'catalog', component: Catalog },
-    { path: 'new', component: NewTheme },
-    { path: 'login', component: Login },
-    { path: 'register', component: Register }
+    { path: '', loadComponent: () => import('./features/home/home').then(c => c.Home) },
+    { path: 'catalog', loadComponent: () => import('./features/catalog/catalog').then(c => c.Catalog) },
+    { path: 'new', loadComponent: () => import('./features/new-theme/new-theme').then(c => c.NewTheme) },
+    { path: 'login', loadComponent: () => import('./features/login/login').then(c => c.Login) },
+    { path: 'register', loadComponent: () => import('./features/register/register').then(c => c.Register) },
+    { path: 'profile', loadComponent: () => import('./features/profile/profile').then(c => c.Profile) },
+    { path: '**', loadComponent: () => import('./shared/components/not-found/not-found').then(c => c.NotFound) }
 ];
