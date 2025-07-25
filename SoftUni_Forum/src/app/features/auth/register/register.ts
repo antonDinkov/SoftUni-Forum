4f5d65990b4e7e7931a4b84c1e67a51b5a378041
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-25 15:12:41 +0300

    trying to fix error msg problem, so far no success

--- SoftUni_Forum/src/app/features/auth/register/register.ts
+++ SoftUni_Forum/src/app/features/auth/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../../../core/services/auth';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
@@ -12,7 +12,8 @@ import { CommonModule } from '@angular/common';
 })
 export class Register {
     private auth = inject(Auth);
-    private router = inject(Router)
+    private router = inject(Router);
+    private usernameErrorsAvailable = signal(false);
 
     form: FormGroup;
 
@@ -21,16 +22,63 @@ export class Register {
             username: ['', [Validators.required, Validators.minLength(5)]],
             email: ['', [Validators.required, Validators.email]],
             tel: ['', [Validators.required]],
-            password: ['', [Validators.required, Validators.minLength(5)]],
-            repass: ['', [Validators.required]]
-        }, { validators: this.passwordMatch });
+            passwords: this.formBuilder.nonNullable.group({
+                password: ['', [Validators.required, Validators.minLength(5)]],
+                repass: ['', [Validators.required]]
+            })
+        }/* , { validators: this.passwordMatch } */);
     }
-
-    passwordMatch(group: AbstractControl): ValidationErrors | null {
+    /* passwordMatch(group: AbstractControl): ValidationErrors | null {
         const pass = group.get('password')?.value;
         const repass = group.get('repass')?.value;
         return pass === repass ? null : { noMatch: true };
+    } */
+
+    get username(): AbstractControl<any, any> | null {
+        return this.form.get('username');
+    };
+
+    get isUsernameInvalid(): boolean {
+        return !!(this.username?.invalid && (this.username.touched || this.username.dirty));
     }
+
+    get invalidUsernameMsg(): string {
+        console.log(this.username);
+        const errors = this.username?.errors;
+
+        if (errors) {
+            this.usernameErrorsAvailable.set(true);
+        }
+
+        if (!this.usernameErrorsAvailable()) {
+            return '';
+        }
+
+        if (errors?.['required']) {
+            return 'Username is required!';
+        }
+
+        if (errors?.['minlength']) {
+            return 'Username should have at least 5 characters!';
+        }
+
+        return '';
+        /* if (this.username?.errors?.['required']) {
+            console.log(this.username?.errors);
+            return 'Username is required!';
+        }
+
+        if (this.username?.errors?.['minlength']) {
+            return 'Username should have at least 5 characters!';
+        }
+
+        return ''; */
+    }
+
+    /* onUsernameBlur() {
+        this.username?.markAsTouched();
+        this.username?.updateValueAndValidity({ onlySelf: true });
+    } */
 
     register() {
         if (this.form.invalid) return;
