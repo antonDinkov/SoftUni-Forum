46d5d55e0411f566e43ef57e41f2d60bd798d551
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-28 09:59:49 +0300

    login controler ready

--- SoftUni_Forum/src/app/features/auth/login/login.ts
+++ SoftUni_Forum/src/app/features/auth/login/login.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Auth } from '../../../core/services/auth';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -19,10 +19,60 @@ export class Login {
 
     constructor(private fb: FormBuilder) {
         this.form = this.fb.group({
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(4)]]  // ✅ поправен key
+            email: ['', [Validators.required, Validators.email, this.emailValidationFn()]],
+            password: ['', [Validators.required, Validators.minLength(5)]]  // ✅ поправен key
         });
     }
+
+    emailValidationFn(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const value = control.value;
+            const regex = /.{6,}@gmail\.(bg|com)/g;
+            const isValid = regex.test(value);
+            return isValid ? null : { emailInvalid: true }
+        }
+    }
+
+    get email(): AbstractControl<any, any> | null {
+        return this.form.get('email');
+    };
+    get isEmailInvalid(): boolean {
+        return !!(this.email?.invalid && (this.email?.touched || this.email?.dirty));
+    };
+    get invalidEmailMsg (): string {
+        if (this.email?.touched && this.email?.errors?.['required']) {
+            return 'Email is reqquired!'
+        }
+
+        if (this.email?.touched && this.email?.errors?.['email']) {
+            return 'This is not a valid email address!'
+        }
+
+        if (this.email?.errors?.['emailInvalid']) {
+            return 'Wrong email!'
+        }
+
+        return '';
+    }
+
+    get password(): AbstractControl<any, any> | null {
+        return this.form.get('password');
+    };
+    get isPasswordInvalid(): boolean {
+        return !!(this.password?.invalid && (this.password?.touched || this.password?.dirty));
+    };
+    get invalidPasswordMsg (): string {
+        if (this.password?.touched && this.password?.errors?.['required']) {
+            return 'Password is reqquired!'
+        }
+
+        if (this.password?.touched && this.password?.errors?.['minlength']) {
+            return 'Password must be atleast 5 characters long!'
+        }
+
+        return '';
+    }
+
 
     login() {
         if (this.form.invalid) return;
